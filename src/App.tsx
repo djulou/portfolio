6186b3d6b09ddf32ef8593553a286851826ac1f5
djulou <dorian.julou@gmail.com>
2025-11-18 15:15:21 +0100

    Remove data

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 //import { useState, useEffect, useRef } from 'react';
-import portfolioData from './data/data.json';
+//import portfolioData from './data/data.json';
 
-import { PortfolioData } from './data/types.ts';
+//import { PortfolioData } from './data/types.ts';
 import Header from './components/Header';
 import Hero from './components/Hero';
 
 const App = () => {
-  const data: PortfolioData = portfolioData;
+  //const data: PortfolioData = portfolioData;
 
   return (
     <>
